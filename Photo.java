ebd98e7d2e012147be2557a837d3fd7a8fb7906f
Polaryti <45695029+Polaryti@users.noreply.github.com>
2019-02-28 19:29:59 +0100

    Add files via upload

--- Photo.java
+++ Photo.java
@@ -1,6 +1,7 @@
 public class Photo {
     public boolean tipo;
     public String[] tags;
+    public int[] pos;
 
     public Photo(String p, int n, String[] ar) {
         if (p.equals("H")) {
@@ -13,7 +14,17 @@ public class Photo {
         for (int k = 0; k < n; k++) {
             tags[k] = ar[k];
         }
+
+        pos = new int[2];
     } 
+
+    public void setX(int x) {
+        pos[0] = x;
+    }
+
+    public void setY(int y) {
+        pos[1] = y;
+    }
 
     public void toStrin() {
         System.out.println(tags.length);
