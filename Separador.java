15b1f343353498336ebafb2dc2c573648b473c0e
agranadosb <34869791+agranadosb@users.noreply.github.com>
2019-02-28 20:49:15 +0100

    Add files via upload

---
+++ Separador.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.LinkedList;
+/**
+ * Separador
+ */
+
+public class Separador {
+
+    public static LinkedList<Photo> getV(LinkedList<Photo> photos) {
+        LinkedList<Photo> vertical = new LinkedList<Photo>();
+        Photo aux;
+
+        for (int i = 0; i < photos.size(); i++) {
+            aux = photos.get(i);
+            if (aux.tipo) {
+                aux.setX(i);
+                vertical.add(aux);
+            }
+        }
+
+        return vertical;
+    }
+
+    public static LinkedList<Photo> getH(LinkedList<Photo> photos) {
+        LinkedList<Photo> horizontal = new LinkedList<Photo>();
+        Photo aux;
+
+        for (int i = 0; i < photos.size(); i++) {
+            aux = photos.get(i);
+            if (!aux.tipo) {
+                aux.setX(i);
+                horizontal.add(aux);
+            }
+        }
+
+        return horizontal;
+    }
+
+    public static LinkedList<Photo> merge(LinkedList<Photo> h, LinkedList<Photo> v) {
+        LinkedList<Photo> res;
+        if (h.size() > v.size()){
+            res = h;
+            for (int i = 0; i < v.size(); i++) {
+                res.add(v.get(i));
+            }
+        }
+        else {
+            res = v;
+            for (int i = 0; i < h.size(); i++) {
+                res.add(h.get(i));
+            }
+        }
+        return res;
+        
+    }
+}
